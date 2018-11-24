a326784b6c727a19ece7676f19c01354df26256c
Govind Venkatesh <venkateshgovind1@gmail.com>
2018-11-24 17:29:08 +0000

    exploration into any type

--- ECS/src/tests/mod.rs
+++ ECS/src/tests/mod.rs
@@ -2,11 +2,22 @@ use entity::*;
 use entity::management::*;
 use component::*;
 
-struct StubComponent{
+struct StubComponentA {
     pub counter: u8
 }
 
-impl Component for StubComponent{
+impl Component for StubComponentA {
+    fn update(&mut self) {
+        self.counter += 1;
+    }
+}
+
+struct StubComponentB {
+    pub counter: u8
+}
+
+impl Component for StubComponentB{
+
     fn update(&mut self) {
         self.counter += 1;
     }
@@ -14,14 +25,14 @@ impl Component for StubComponent{
 
  #[test]
  fn initialise_ecs(){
-     let entity_manager:EntityStorage<StubComponent> = EntityStorage::new();
+     let entity_manager:EntityStorage<StubComponentA> = EntityStorage::new();
      assert_eq!(entity_manager.generation, 0);
      assert_eq!(entity_manager.storage.len(), 0);
  }
 
  #[test]
  fn create_new_empty_entity(){
-     let mut entity_manager: EntityStorage<StubComponent> = EntityStorage::new();
+     let mut entity_manager: EntityStorage<StubComponentA> = EntityStorage::new();
      let (entity, generation) = entity_manager.register_new_entity();
      assert_eq!(generation, 1);
      assert_eq!(entity, 0);
@@ -29,18 +40,18 @@ impl Component for StubComponent{
 
  #[test]
  fn add_component_to_entity(){
-     let mut entity_manager: EntityStorage<StubComponent> = EntityStorage::new();
+     let mut entity_manager: EntityStorage<StubComponentA> = EntityStorage::new();
      let index = entity_manager.register_new_entity();
-     let (index, gen) = entity_manager.add_component(index, StubComponent{counter:0}).unwrap();
+     let (index, gen) = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
      assert_eq!(gen, 2);
      assert_eq!(index, 0);
  }
 
  #[test]
  fn remove_component_from_entity(){
-     let mut entity_manager: EntityStorage<StubComponent> = EntityStorage::new();
+     let mut entity_manager: EntityStorage<StubComponentA> = EntityStorage::new();
      let index = entity_manager.register_new_entity();
-     let index = entity_manager.add_component(index, StubComponent{counter:0}).unwrap();
+     let index = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
      let (ind, gen) = entity_manager.remove_component(index).unwrap();
      assert_eq!(gen, 3);
      assert_eq!(ind, 0);
@@ -48,9 +59,17 @@ impl Component for StubComponent{
 
  #[test]
  fn remove_entity_from_ecs(){
-     let mut entity_manager: EntityStorage<StubComponent> = EntityStorage::new();
+     let mut entity_manager: EntityStorage<StubComponentA> = EntityStorage::new();
      let index = entity_manager.register_new_entity();
-     let index = entity_manager.add_component(index, StubComponent{counter:0}).unwrap();
+     let index = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
      let result = entity_manager.deregister_entity(index);
      assert!(result.is_ok(), true);
  }
+
+#[test]
+fn register_new_component(){
+    //issue with dynamic typing.
+    //look into any type
+    //any type is the solution here, but need to figure out how to implement it into the EntityManager
+}
+
