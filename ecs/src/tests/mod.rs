7654c1a7b20a9c4489e53aff1107d938f2035bec
Govind Venkatesh <venkateshgovind1@gmail.com>
2018-12-21 17:46:31 +0000

    tests needs updating

--- ecs/src/tests/mod.rs
+++ ecs/src/tests/mod.rs
@@ -25,56 +25,57 @@ impl Component for StubComponentB{
     }
 }
 
- #[test]
- fn initialise_ecs(){
-     let entity_manager:EntityStorage = EntityStorage::new();
-     assert_eq!(entity_manager.entity_list.len(), 0);
-     assert_eq!(entity_manager.free_list.len(), 0);
-     assert_eq!(entity_manager.storage.len(), 0);
- }
+#[test]
+fn initialise_ecs(){
+    let entity_manager:EntityStorage = EntityStorage::new();
+    assert_eq!(entity_manager.entity_list.len(), 0);
+    assert_eq!(entity_manager.free_list.len(), 0);
+    assert_eq!(entity_manager.storage.len(), 0);
+}
 
- #[test]
- fn create_new_empty_entity(){
-     let mut entity_manager: EntityStorage = EntityStorage::new();
-     let (entity, generation) = entity_manager.allocate_new_entity();
-     assert_eq!(generation, 0);
-     assert_eq!(entity, 0);
-     assert_eq!(entity_manager.size, 1);
- }
+#[test]
+fn create_new_empty_entity(){
+    let mut entity_manager: EntityStorage = EntityStorage::new();
+    let (entity, generation) = entity_manager.allocate_new_entity();
+    assert_eq!(generation, 0);
+    assert_eq!(entity, 0);
+    assert_eq!(entity_manager.size, 1);
+}
 
- #[test]
- fn add_component_to_entity(){
-     let mut entity_manager: EntityStorage = EntityStorage::new();
-     let index = entity_manager.allocate_new_entity();
-     entity_manager.register_new_component::<StubComponentA>();
-     let (index1, gen) = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
-     assert_eq!(gen, 1);
-     assert_eq!(index1, 0);
- }
+#[test]
+fn add_component_to_entity(){
+    let mut entity_manager: EntityStorage = EntityStorage::new();
+    let index = entity_manager.allocate_new_entity();
+    entity_manager.register_new_component::<StubComponentA>();
+    let (index1, gen) = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
+    assert_eq!(gen, 1);
+    assert_eq!(index1, 0);
+}
 
- #[test]
- fn remove_component_from_entity(){
-     let mut entity_manager: EntityStorage = EntityStorage::new();
-     let index = entity_manager.allocate_new_entity();
-     entity_manager.register_new_component::<StubComponentA>();
-     let index = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
-     let (ind, gen) = entity_manager.remove_component::<StubComponentA>(index).unwrap();
-     assert_eq!(gen, 2);
-     assert_eq!(ind, 0);
- }
+#[test]
+fn remove_component_from_entity(){
+    let mut entity_manager: EntityStorage = EntityStorage::new();
+    let index = entity_manager.allocate_new_entity();
+    entity_manager.register_new_component::<StubComponentA>();
+    let index = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
+    let (ind, gen) = entity_manager.remove_component::<StubComponentA>(index).unwrap();
+    assert_eq!(gen, 2);
+    assert_eq!(ind, 0);
+}
 
- #[test]
- fn remove_entity_from_ecs(){
-     let mut entity_manager: EntityStorage = EntityStorage::new();
-     let index = entity_manager.allocate_new_entity();
-     entity_manager.register_new_component::<StubComponentA>();
-     let index = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
-     let result = entity_manager.deallocate_entity(index);
-     assert!(result.is_ok(), true);
- }
+#[test]
+fn remove_entity_from_ecs(){
+    let mut entity_manager: EntityStorage = EntityStorage::new();
+    let index = entity_manager.allocate_new_entity();
+    entity_manager.register_new_component::<StubComponentA>();
+    let index = entity_manager.add_component(index, StubComponentA {counter:0}).unwrap();
+    let result = entity_manager.deallocate_entity(index);
+    assert!(result.is_ok(), true);
+}
 
 #[test]
 fn register_new_component(){
+    //
     let mut entity_manager = EntityStorage::new();
     let res1 = entity_manager.allocate_new_entity();
     let res2 = entity_manager.register_new_component::<StubComponentA>().unwrap();
@@ -83,6 +84,7 @@ fn register_new_component(){
 
 #[test]
 fn get_component_test(){
+    //
     let mut entity_manager = EntityStorage::new();
     let res1 = entity_manager.allocate_new_entity();
     entity_manager.register_new_component::<StubComponentA>();
