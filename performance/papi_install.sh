a5e4023a7fd8fb1667254a363544cc4d9c26b2a7
Govind Venkatesh <venkateshgovind1@gmail.com>
2019-02-26 12:09:58 +0000

    install script for papi and travis config change

---
+++ performance/papi_install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+mkdir -p papi
+cd papi
+wget http://icl.cs.utk.edu/projects/papi/downloads/papi-5.2.0.tar.gz
+tar xzf papi-5.2.0.tar.gz
+cd papi-5.2.0/src
+./configure --prefix=/usr
+make
+sudo make install
