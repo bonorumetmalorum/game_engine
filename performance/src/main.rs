869fb35632a6b347cc21c3281556453c0c73adb0
Govind Venkatesh <venkateshgovind1@gmail.com>
2019-03-19 19:08:57 +0000

    cache ramping up performance test complete

--- performance/src/main.rs
+++ performance/src/main.rs
@@ -15,13 +15,16 @@ use gnuplot::Major;
 use gnuplot::Color;
 use gnuplot::BorderColor;
 use gnuplot::Fix;
+use gnuplot::Graph;
 use gnuplot::AxesCommon;
 use gnuplot::Auto;
+use gnuplot::Caption;
 use ecs::component::dense_component_storage::DenseComponentStorage;
 use ecs::component::dense_component_storage::DenseComponentIterator;
 use ecs::component::dense_component_storage::DenseComponentIteratorMut;
 use ecs::component::iter::Iter;
 use ecs::component::storage::Storage;
+use gnuplot::Mirror;
 
 
 const STANDARD: usize = 10000;
@@ -33,10 +36,6 @@ struct R{
 
 impl Component for R{
     type ComponentStorage = DenseComponentStorage<Self>;
-
-    fn update(&mut self) {
-        unimplemented!()
-    }
 }
 
 #[derive(Clone)]
@@ -46,10 +45,6 @@ struct W1{
 
 impl Component for W1{
     type ComponentStorage = DenseComponentStorage<Self>;
-
-    fn update(&mut self) {
-        unimplemented!()
-    }
 }
 
 #[derive(Clone)]
@@ -59,9 +54,6 @@ struct W2{
 
 impl Component for W2{
     type ComponentStorage = DenseComponentStorage<Self>;
-
-    fn update(&mut self) {
-    }
 }
 
 fn build(number: usize) -> (Vec<EntityIndex>, ECS) {
@@ -73,8 +65,8 @@ fn build(number: usize) -> (Vec<EntityIndex>, ECS) {
     (entities, ecs)
 }
 
-fn setup() -> ECS {
-    let (entities, mut ecs) = build(STANDARD);
+fn setup(sample_size: usize) -> ECS {
+    let (entities, mut ecs) = build(sample_size);
     ecs.register_new_component::<R>().expect("unable to register new component");
     ecs.register_new_component::<W1>().expect("unable to register new component");
     ecs.register_new_component::<W2>().expect("unable to register new component");
@@ -85,6 +77,8 @@ fn setup() -> ECS {
     }
     ecs
 }
+
+
 
 fn system_w1(read_r: DenseComponentIterator<R>, write_w1: DenseComponentIteratorMut<W1>) {
     let joint = read_r.join(write_w1);
@@ -145,7 +139,7 @@ fn main() {
             papi::CounterSet::new(counters)
         };
         let start = counters.read();
-        let mut ecs = setup();
+        let mut ecs = setup(STANDARD);
         let stop = counters.accum();
 
         println!("allocated 10000 entities with components with {} L1 misses, {} L2 misses",
@@ -177,7 +171,7 @@ fn main() {
         let mut counters = unsafe {
             papi::CounterSet::new(counters)
         };
-        let mut ecs = setup();
+        let mut ecs = setup(STANDARD);
         let hr1 = ecs.get::<R>();
         let hr2 = ecs.get::<R>();
         let mut hw1 = ecs.get_mut::<W1>();
@@ -210,6 +204,56 @@ fn main() {
                 &[],
             )
             .set_y_range(Fix(0.0), Auto);
+        fg.show();
+    }
+
+    {
+        //ramp up test - total sample size will be 10000
+        const NUM_SAMPLE: usize = 10000;
+        const NUM_STEP: usize = 1000;
+        let mut l1cachemiss: Vec<i64> = Vec::new();
+        let mut l2cachemiss: Vec<i64> = Vec::new();
+        let mut xaxis: Vec<usize> = Vec::new();
+        let counters = &[papi::Counter::PAPI_L1_DCM, papi::Counter::PAPI_L2_DCM];
+        let mut counters = unsafe {
+            papi::CounterSet::new(counters)
+        };
+
+        for i in (0..NUM_SAMPLE).step_by(NUM_STEP){
+            //setup
+            let mut ecs = setup(i);
+            let hr1 = ecs.get::<R>();
+            let hr2 = ecs.get::<R>();
+            let mut hw1 = ecs.get_mut::<W1>();
+            let mut hw2 = ecs.get_mut::<W2>();
+            let itrr1 = hr1.get_iter();
+            let itrr2 = hr2.get_iter();
+            let itrw1 = hw1.get_mut_iter();
+            let itrw2 = hw2.get_mut_iter();
+            //collection of data
+            let start = counters.read();
+            system_w1(itrr1, itrw1);
+            system_w2(itrr2, itrw2);
+            let stop = counters.accum();
+            xaxis.push(i);
+            //store results
+            l1cachemiss.push(stop[0] - start[0]);
+            l2cachemiss.push(stop[1] - start[1]);
+        }
+        //plotting
+
+        for el in l2cachemiss.iter().zip(xaxis.iter()) {
+            println!("{}, {}", el.0, el.1);
+        }
+        let mut fg = Figure::new();
+        fg.set_terminal("pngcairo", "./data/l1andl2cachemissscalability.png");
+        fg.axes2d()
+            .set_title("L1 and L2 cache miss scalability", &[])
+            .set_legend(Graph(1.0), Graph(0.5), &[], &[])
+            .lines(xaxis.iter(), l1cachemiss.iter(), &[Caption("L1 cache misses"), Color("blue")])
+            .lines(xaxis.iter(), l2cachemiss.iter(), &[Caption("L2 cache misses"), Color("red")])
+            .set_x_ticks(Some((Fix(1000.0), 1)), &[], &[])
+            .set_y_ticks(Some((Auto, 1)), &[], &[]);
         fg.show();
     }
 }
